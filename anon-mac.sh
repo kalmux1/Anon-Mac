1cadf7b343f363e5dedd5f65976292c2f76df4d3
kalmux1 <kal.mux.cyber@gmail.com>
2023-12-19 00:28:21 +0530

    v1.0

--- anon-mac.sh
+++ anon-mac.sh
@@ -7,11 +7,10 @@ tool=$(sudo apt-get install macchanger)
 # Storing details from ifconfig command to mac.txt file
 
 file=$(ifconfig -s > if.txt)
-echo "$file"
 
-#Infc=$(grep -n 2 if.txt)
+Infc=$(grep -n 2 if.txt)
 
-#echo "$Infc"
+echo $Infc
 
 
 
