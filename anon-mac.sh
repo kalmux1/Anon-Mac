4030edad06360a1f9607bec01397e0027e8e7116
kalmux1 <kal.mux.cyber@gmail.com>
2023-12-19 01:23:01 +0530

    v1.0

--- anon-mac.sh
+++ anon-mac.sh
@@ -4,24 +4,26 @@
 
 tool=$(sudo apt-get install macchanger)
 
-# Storing details from ifconfig command to mac.txt file
+# Storing details from ifconfig command to if.txt file
 
 file=$(ifconfig -s > if.txt)
+
+# grepping 2nd line wich contain interface value
 
 Infc=$(grep -n 2 if.txt)
 
 echo $Infc
 
+# storing interface name into cl var 
+
 cl=(${Infc:2:10})
 
 echo $cl
 
-#interface=${Infc:2:10}
-#echo $interface
 
-$(macchanger -s $cl)
+#$(macchanger -s $cl)
 
-echo $Mac
+#echo $Mac
 
 
 
