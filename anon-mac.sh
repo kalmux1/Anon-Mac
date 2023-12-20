ef44d562d522ed05a0a559011e1150e076085f98
kalmux1 <kal.mux.cyber@gmail.com>
2023-12-21 01:02:28 +0530

    v1.0

--- anon-mac.sh
+++ anon-mac.sh
@@ -4,13 +4,13 @@
 
 echo " "
 apt=$(sudo apt-get install figlet)
-figlet -f slant.flf -c "Anon-Mac" 
+figlet -f slant.flf  "Anon-Mac" 
 
 # Author Details
 
-echo "                         Cretaed By KALMUX       v1.0"
-echo "                Installing required packages for your system        "
-echo "             This may take some time based on your Internet speed    "
+echo "                  Cretaed By KALMUX       v1.0"
+echo "    Installing required packages for your system        "
+echo "This may take some time based on your Internet speed    "
 echo " "
 
 #Intsalling Macchanger tool
@@ -35,38 +35,38 @@ INFC=$(awk 'NR == 2 {print $1}' logs/infs.txt > logs/main_inf.txt )
 cl=$(cat logs/main_inf.txt)
 
 # showing current mac and permanent mac using macchanger
-echo "-----------------------------------------------"
+echo "-------------------------------------------------"
 echo "       Current Macc Address Details"
 echo " "
 sudo macchanger -s "$cl" 
 sleep 2s
-echo "-----------------------------------------------"
+echo "-------------------------------------------------"
 echo " "
 
 # Layer 1 Encryption 
-echo "-----------------------------------------------"
+echo "-------------------------------------------------"
 echo "  Encrypting Your Macc Address ====> {Level 1}"
 echo " "
 sudo macchanger -r "$cl"
 sleep 2s
-echo "-----------------------------------------------"
+echo "-------------------------------------------------"
 echo " "
 
 # Layer 2 Encryption 
-echo "-----------------------------------------------"
-echo "Encrypting Your Macc Address ====> {Level 2}"
+echo "-------------------------------------------------"
+echo " Encrypting Your Macc Address ====> {Level 2}"
 echo " "
 sudo macchanger -r "$cl"
 sleep 2s
-echo "-----------------------------------------------"
+echo "-------------------------------------------------"
 echo " "
 
 # Layer 3 Encryption 
-echo "-----------------------------------------------"
-echo "Encrypting Your Macc Address ====> {Level 3}"
+echo "-------------------------------------------------"
+echo " Encrypting Your Macc Address ====> {Level 3}"
 echo " "
 sudo macchanger -r "$cl"
-echo "-----------------------------------------------"
+echo "-------------------------------------------------"
 
 # Clearing logs 
 
